2d760ec1bde7843c159686d3b8e145dfbec1cf7c
Lev Berman <ldmberman@gmail.com>
2019-01-04 15:24:41 +0100

    Add block_height system tag to app_search txs

--- src/apps/app_search.erl
+++ src/apps/app_search.erl
@@ -88,6 +88,7 @@ update_tag_table(B) when ?IS_BLOCK(B) ->
 			add_entry(<<"to">>, ar_util:encode(ar_wallet:to_address(TX#tx.target)), TX#tx.id),
 			add_entry(<<"quantity">>, TX#tx.quantity, TX#tx.id),
 			add_entry(<<"reward">>, TX#tx.reward, TX#tx.id),
+			add_entry(<<"block_height">>, B#block.height, TX#tx.id),
 			add_entry(<<"block_indep_hash">>, ar_util:encode(B#block.indep_hash), TX#tx.id)
 		end,
 		ar_storage:read_tx(B#block.txs)
